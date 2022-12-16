4b194500e156a0f6a84e0d6104d838596633711b
Thomas Leiter <thomas.leiter@gmx.de>
2022-12-16 12:11:35 +0100

    SQL Scripts Added/Renamed.

---
+++ Bezahlautomat/VORGANGS_DATEN_CREATE.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[VORGANGS_DATEN] (
+    [Id]           INT      IDENTITY (1, 1) NOT NULL,
+    [Datum]        DATETIME NULL,
+    [BetragInCent] INT      DEFAULT ((0)) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
